4d2105b8885ea8303585071c6d055ffa12011172
Kristaps Erglis <kristaps.erglis@gmail.com>
2014-01-23 22:32:21 +0200

    StdCrud fix

--- app/models/std_crud.rb
+++ app/models/std_crud.rb
@@ -18,13 +18,13 @@ module StdCrud
 
       def create
         create! do |success, failure|
-          success.html { redirect_to edit_resource_url }
+          success.html { redirect_to collection_url }
         end
       end
 
       def update
         update! do |success, failure|
-          success.html { redirect_to edit_resource_url }
+          success.html { redirect_to collection_url }
         end
       end
 
